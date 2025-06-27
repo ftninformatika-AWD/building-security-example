05b75b71457f7beaf59dc6ecb6842cacff306132
Vanja Mijatov <v.mijatov@gmail.com>
2025-06-27 19:43:50 +0200

    Simplify collection mapping

--- BuildingExample/BuildingExample/Validators/ApartmentValidator.cs
+++ BuildingExample/BuildingExample/Validators/ApartmentValidator.cs
@@ -3,12 +3,18 @@ using BuildingExample.Exceptions;
 
 namespace BuildingExample.Validators
 {
+    // manuelna validacija putem Validator klase koja je smeštena u Validators paket
     public class ApartmentValidator
     {
+        // kada je neophodno izvršiti validaciju objekta na način koji nije podržan
+        // Data anotacijama, najjednostavniji način je napraviti Validator klasu sa
+        // metodama koje vrše validaciju na način koji mi želimo i pozivati ih manuelno
         public static void ValidateSearchApartment(ApartmentSearchDTO dto)
         { 
             if (dto.AreaFrom > dto.AreaTo)
             {
+                // ako validacija nije uspešna, baciti izuzetak koji sadrži razlog
+                // zašto je validacija neuspešna
                 throw new InvalidAreaBadRequestException();
             }
         }
